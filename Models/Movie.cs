17cd2c5956511da861edfb7219e7641ffc8d8d64
Георгий Жаринов <not.ban55@mail.ru>
2023-06-08 15:30:00 +0300

    Добавлены аттрибуты для модели movies

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebSiteMovie.Models
 {
@@ -8,11 +10,13 @@ namespace WebSiteMovie.Models
 
         public string? Title { get; set; }
 
+        [DisplayName("Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
         public string? Genre { get; set; }
 
+        [Column(TypeName ="decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }

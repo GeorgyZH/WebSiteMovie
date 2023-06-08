964f4e1b91bffb39b9a16afd99614b47e66b4387
Георгий Жаринов <not.ban55@mail.ru>
2023-06-08 12:02:08 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSiteMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}

ca00397992766af8aba9bf9388b068bee508b8e0
Георгий Жаринов <not.ban55@mail.ru>
2023-06-08 12:07:51 +0300

    Новый метод welcome в контроллере HelloWorld

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -1,18 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace WebSiteMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
+        // GET: /HelloWorld/
         public string Index()
         {
             return "This is my default action...";
         }
+
         // 
         // GET: /HelloWorld/Welcome/ 
-        public string Welcome()
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         }
     }
 }
